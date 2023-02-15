57b1ec7a1cfb9c3a888cde937db4f10e5258891f
Philip <philip.moran.2009@gmail.com>
2023-02-15 15:10:14 +0100

    adding all the modules

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,6 +1,7 @@
 //import * as THREE from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three'
 
 // import {OrbitControls} from '/three/examples/jsm/controls/OrbitControls.js'
 // import { OrbitControls } from '../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
